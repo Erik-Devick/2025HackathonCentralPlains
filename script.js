664ed72c54818c1135dbd9d04801b4f3b379ffd1
erik_devick <erikdevick1@gmail.com>
2025-02-22 15:43:37 -0600

    commit

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function login() {
     window.location.href = "dashboard.html";
 }
+
+function addCourse(course) {
+
+}
+
+class Course {
+    constructor(name, startRow, startCol, endRow, endCol) {
+        this.name = name;
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.endRow = endRow;
+        this.endCol = endCol;
+    }
+}
+
+document.querySelectorAll(".course").forEach(course => {
+    course.addEventListener("click", function() {
+        // Remove previous selection
+        document.querySelectorAll(".course").forEach(c => c.classList.remove("selected"));
+        // Highlight clicked course
+        this.classList.add("selected");
+    });
+});
